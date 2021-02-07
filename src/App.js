0e54286bac68214284177e1f27f5870b71f95424
Sckrool <sckrool@gmail.com>
2021-02-06 20:05:36 -0600

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 
 import './App.css';
+import React from 'react';
+
+function Holamundo(props) {
+  return(
+    <div id="hola">
+      <h3>{props.subtitulo}</h3>
+      {props.texto}
+    </div>
+  );
+}
+
+// const App = () => <div>El componente: <Holamundo/></div>
+
+// class App extends React.Component{
+//   render(){
+//     return <div>El componente: <Holamundo/></div>
+//   }
+// }
 
 function App() {
   return (
-    <div>Hola Mundo</div>
+    <div>
+      El Componente: 
+      <Holamundo texto="Hola Sck" subtitulo="subTitulo1"/>
+      <Holamundo texto="Hola Manuelito"/>
+      <Holamundo texto="Hola Mayra"/>
+    </div>
   );
 }
 
