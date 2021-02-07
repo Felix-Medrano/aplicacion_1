f9e1093cc1261c1e70c77a6eec13a0ef9d72212d
Sckrool <sckrool@gmail.com>
2021-02-06 18:49:34 -0600

    prueba 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // agregando comentario para probar git
+// otra mas
 ReactDOM.render(
   <React.StrictMode>
     <App />
