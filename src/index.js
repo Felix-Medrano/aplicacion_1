f87ee76539c2768aeb81ade3ee39e28ca1367aea
Sckrool <sckrool@gmail.com>
2021-02-06 19:20:33 -0600

    Eliminando textos de prueba

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// agregando comentario para probar git
-// otra mas
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
